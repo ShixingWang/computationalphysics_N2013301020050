380d42856fa0012c61554344cb46b08895e156f4
Shixing Wang <wangshixing@whu.edu.cn>
2016-04-11 15:16:43 +0800

    Coriolis Effect concerned

--- Codes/Exercise7_2.py
+++ Codes/Exercise7_2.py
@@ -8,26 +8,33 @@ y_zero=10000
 a=0.0065
 T0=300
 alpha=2.5
+Coriolis=0.1
+omega=0.0000727
 # set constants
 class cannon0:
     "the simplest model with no air drag, no air density variance, no probability distribution"
     # initialize variables    
-    def __init__(self,v0,theta,yFinal=0):
+    def __init__(self,v0=600,theta=45,psi=60,latitude=45,yFinal=0):
+        self.latitude=latitude*math.pi/180       
+        self.psi=psi*math.pi/180 # the angle from East, degree to rad
         self.x0=0
         self.y0=0
         self.z0=0
         self.yFinal=yFinal
         self.v0=v0
         self.Theta=theta
-        self.theta=theta*math.pi/180
-        self.vx0=self.v0*math.cos(self.theta)
-        self.vy0=self.v0*math.sin(self.theta)
-        self.vz0=0
+        self.theta=theta*math.pi/180 # the angle above horizen, degree to rad
+        self.vx0=self.v0*math.cos(self.theta)*math.cos(self.psi)
+        self.vy0=self.v0*math.cos(self.theta)*math.sin(self.psi)
+        self.vz0=self.v0*math.sin(self.theta)
         self.dt=0.01
         return None
     # external force other than gravity, no force in simplest case        
-    def F(self,vx,vy,y=1):
-        return 0,0,0
+    def F(self,vx,vy,vz,y=1):
+        self.Fx=Coriolis*(omega*math.cos(self.latitude)*vz-omega*math.sin(self.latitude)*vy)
+        self.Fy=Coriolis*(omega*math.sin(self.latitude)*vx)
+        self.Fz=-Coriolis*(omega*math.cos(self.latitude)*vx)
+        return self.Fx,self.Fy,self.Fz
     # calculate the flying trajactory
     def fly(self):
         self.X=[self.x0]
@@ -38,9 +45,9 @@ class cannon0:
         self.Vz=[self.vz0]
         self.T=[0]
         while not (self.Y[-1]<self.yFinal and self.Vy[-1]<0):
-            newVx=self.Vx[-1]+self.F(vx=self.Vx[-1],vy=self.Vy[-1])[0]*self.dt
-            newVy=self.Vy[-1]-g*self.dt+self.F(self.Vx[-1],self.Vy[-1])[1]*self.dt
-            newVz=self.Vz[-1]+self.F(vx=self.Vx[-1],vy=self.Vy[-1])[2]*self.dt
+            newVx=self.Vx[-1]+self.F(vx=self.Vx[-1],vy=self.Vy[-1],vz=self.Vz[-1])[0]*self.dt
+            newVy=self.Vy[-1]+self.F(self.Vx[-1],self.Vy[-1],self.Vz[-1])[1]*self.dt
+            newVz=self.Vz[-1]-g*self.dt+self.F(vx=self.Vx[-1],vy=self.Vy[-1],self.Vz[-1])[2]*self.dt
             self.Vx.append(newVx)
             self.Vy.append(newVy)
             self.Vz.append(newVz)
@@ -83,40 +90,41 @@ class cannon0:
         zlabel = vis.label(text = 'z', pos = (0, 0, 0), axis = (0,0,100))
         while 1:
             pass
-
         
 class cannon1(cannon0):
     "the second simplest model with no air drag under constant air density, no probability distribution"    
     # external force other than gravity        
-    def F(self,vx,vy,y=1):
-        vl=math.sqrt(vx**2+vy**2)
-        self.Fx=-B2m*vx*vl
-        self.Fy=-B2m*vy*vl
-        return self.Fx,self.Fy
+    def F(self,vx,vy,vz,y=1):
+        vl=math.sqrt(vx**2+vy**2+vz**2) # total speed
+        self.Fx=-B2m*vx*vl+Coriolis*(omega*math.cos(self.theta)*vz-omega*math.sin(self.theta)*vy)
+        self.Fy=-B2m*vy*vl+Coriolis*(omega*math.sin(self.theta)*vx)
+        self.Fz=-B2m*vz*vl-Coriolis*(omega*math.cos(self.theta)*vx)
+        return self.Fx,self.Fy,self.Fz
     def plot(self, color):
         plt.plot(self.X,self.Y,color,label="$%dm/s$,$%d\degree$, uniform air drag"%(self.v0,self.Theta))
         return 0
 class cannon2(cannon0):
     "the model concerning ISOTHERMAL air density variance but no probability distribution"
-    def F(self,vx,vy,x=1,y=1):
-        vl=math.sqrt(vx**2+vy**2)
-        self.Fx=-B2m*vx*vl*math.exp(-y/y_zero)
-        self.Fy=-B2m*vy*vl*math.exp(-y/y_zero)
-        return self.Fx,self.Fy
+    def F(self,vx,vy,vz,x=1,y=1):
+        vl=math.sqrt(vx**2+vy**2+vz**2) # total speed
+        self.Fx=-B2m*vx*vl*math.exp(-y/y_zero)+Coriolis*(omega*math.cos(self.theta)*vz-omega*math.sin(self.theta)*vy)
+        self.Fy=-B2m*vy*vl*math.exp(-y/y_zero)+Coriolis*(omega*math.sin(self.theta)*vx)
+        self.Fz=-B2m*vz*vl*math.exp(-y/y_zero)-Coriolis*(omega*math.cos(self.theta)*vx)
+        return self.Fx,self.Fy,self.Fz
     def plot(self, color):
         plt.plot(self.X,self.Y,color,label="$%dm/s$,$%d\degree$, isothermal air drag"%(self.v0,self.Theta))
         return 0
 class cannon3(cannon0):
     "the model concerning ADIABATIC air density variance but no probability distribution"    
-    def F(self,vx,vy,y=1):
-        vl=math.sqrt(vx**2+vy**2)
-        self.Fx=-B2m*vx*vl*(1-a*y/T0)**alpha
-        self.Fy=-B2m*vy*vl*(1-a*y/T0)**alpha
-        return self.Fx,self.Fy
+    def F(self,vx,vy,vz,y=1):
+        vl=math.sqrt(vx**2+vy**2+vz**2) # total speed
+        self.Fx=-B2m*vx*vl*(1-a*y/T0)**alpha+Coriolis*(omega*math.cos(self.theta)*vz-omega*math.sin(self.theta)*vy)
+        self.Fy=-B2m*vy*vl*(1-a*y/T0)**alpha+Coriolis*(omega*math.sin(self.theta)*vx)
+        self.Fz=-B2m*vz*vl*(1-a*y/T0)**alpha-Coriolis*(omega*math.cos(self.theta)*vx)
+        return self.Fx,self.Fy,self.Fz
     def plot(self, color):
         plt.plot(self.X,self.Y,color,label="$%dm/s$,$%d\degree$, adiabatic air drag"%(self.v0,self.Theta))
-        return 0
-
+        return 0        
 # select the angle casting the largest distance
 A=cannon0(600,45)
 A.fly()
