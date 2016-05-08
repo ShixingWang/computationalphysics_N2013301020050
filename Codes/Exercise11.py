f626807e343554ca15615e42469c5c0c38382018
Shixing Wang <wangshixing@whu.edu.cn>
2016-05-08 17:13:02 +0800

    pictures of 11

--- Codes/Exercise11.py
+++ Codes/Exercise11.py
@@ -1,13 +1,14 @@
 # import packages
 import math
+import numpy as np
 import matplotlib.pyplot as plt
 # physical constants
-GM=4*math.pi**2
+GM=4*(math.pi**2)
 alpha=0.0008
 perihelion=0.39*(1-0.206) # to remain the oerihelion the same as that of Mercury.
 # begin the class
 class precession:
-    def __init__(self,e=0.206,time=2.5,dt=0.0001):
+    def __init__(self,e=0.206,time=2.,dt=0.0001):
         self.e=e
         self.a=perihelion/(1-e)
         self.x0=self.a*(1+e)
@@ -28,19 +29,20 @@ class precession:
         while self.T[-1]<self.time:
             r=math.sqrt(self.X[-1]**2+self.Y[-1]**2)
             newVx=self.Vx[-1]-(GM*(1+alpha/r**2)*self.X[-1]/r**3)*self.dt
-            self.Vx.append(newVx)
             newX=self.X[-1]+newVx*self.dt
-            self.X.append(newX)
             newVy=self.Vy[-1]-(GM*(1+alpha/r**2)*self.Y[-1]/r**3)*self.dt
-            self.Vy.append(newVy)
             newY=self.Y[-1]+newVy*self.dt
-            self.Y.append(newY)
-            if abs(newX*newVx+newY*newVy)<0.001 and r<self.a:
-                theta=math.acos(newX/r)
-                if newY/r <0:
+            if abs(newX*newVx+newY*newVy)<0.0014 and r<self.a:
+                theta=math.acos(self.X[-1]/r)*180/math.pi
+                if (self.Y[-1]/r)<0:
                     theta=360-theta
+                theta=abs(theta-180)
                 self.ThetaPrecession.append(theta)
                 self.TimePrecession.append(self.T[-1])
+            self.Vx.append(newVx)
+            self.Vy.append(newVy)
+            self.X.append(newX)
+            self.Y.append(newY)
             self.T.append(self.T[-1]+self.dt)
         return 0
     def plot(self,color='k',slogan=''):
@@ -52,6 +54,51 @@ class precession:
         print self.TimePrecession
         return 0
 
-A=precession()
+A=precession(e=0.206,time=5)
 A.calculate()
-A.orientation()
+A.plot(color='r',slogan=r'e=0.206')
+
+B=precession(e=0.5,time=5.0)
+B.calculate()
+B.plot(color='b',slogan='e=0.5')
+plt.legend(loc='upper right',frameon=False)
+
+C=precession(e=0.8,time=25)
+C.calculate()
+C.plot(color='g',slogan='e=0.8')
+plt.legend(loc='upper right',frameon=False)
+
+#plt.xlim(-0.5,0.7)
+plt.xlabel("x [AU]")
+plt.ylabel("y [AU]")
+plt.title("Simulation of the precession of Mercury")
+plt.legend(loc='upper right',frameon=False)
+
+'''
+A=precession(e=0.206,time=2.)
+A.calculate()
+A.orientation(color='k',slogan=r'$\alpha=0.0008$')
+plt.xlabel("Time [yr]")
+plt.ylabel(r"$\theta$ [$\degree$]")
+plt.title("Orbit Orientation versus Time")
+'''
+'''
+PrecessionRate=[8.609, 6.129, 4.183, 2.769, 1.737, 0.999, 0.483, 0.146]
+PrecessionRate=np.array(PrecessionRate)
+Eccentricity=[0.206,0.3,0.4,0.5,0.6,0.7,0.8,0.9]
+Eccentricity=np.array(Eccentricity)
+Lower=[8.536,6.095,4.161,2.750,1.720,0.970,0.483,0.132]
+Lower=np.array(Lower)
+Upper=[8.681,6.162,4.204,2.789,1.755,1.028,0.483,0.160]
+Upper=np.array(Upper)
+yUpper=Upper-PrecessionRate
+yLower=PrecessionRate-Lower
+plt.errorbar(Eccentricity,PrecessionRate,yerr=[yLower,yUpper],fmt='b-',ecolor='r',capthick=2,label='Precession Rate versus Eccentricity')
+plt.xlabel("Eccentricity")
+plt.ylabel(r"Precession Rate [$\degree/yr$]")
+plt.title("Precession Rate versus Eccectricity")
+#x=np.arange(0.2,0.95,0.01)
+#y=13.67*np.sqrt((1-x)**3)/(1+x)
+#plt.plot(x,y,'b',label=r'Fiiting Curve $f(x)=a*\sqrt{(1-x)^3}/(1+x)$')
+#plt.legend(loc='upper right',frameon=False)
+'''
