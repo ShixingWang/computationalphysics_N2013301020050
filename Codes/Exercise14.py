0f49b4b0100c1d7930a182caf7e3b333f4949408
Shixing Wang <wangshixing@whu.edu.cn>
2016-05-30 16:58:26 +0800

    fix code14

--- Codes/Exercise14.py
+++ Codes/Exercise14.py
@@ -1,26 +1,59 @@
 # import packages
-import math
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib import animation 
 # physical constants
-c=1000
+c=1
 r=1
 k=1000
 
 class wave:
-    def __init__(self,x0=0.3,length=1,time=10,dt=0.001):
+    """Gaussian Initial Condition, Fixed Boundary Condition"""
+    def __init__(self,x0=0.3,length=1,time=10,dt=0.01):
         self.x0=x0
         self.length=length
         self.time=time
         self.dt=dt
         self.dx=c*dt/r
-        self.i=length/self.dx
+        self.i=int(length/self.dx)+1
         self.n=time/dt
-        self.X0=np.linspace(0,length,self.i,endpoint=True)
-        self.Y0=np.exp(-k*(self.X0-x0)**2)
+        self.X=np.linspace(0,length,self.i,endpoint=True)
+        self.Y0=np.exp(-k*(self.X-x0)**2)
         return None
     def calculate(self):
-        self.Y=[None for cycle1 in range(self.n+1)]
-        self.Y[0]=self.Y0
-        
-        
+        self.Y=[self.Y0]
+        Y1=np.linspace(0,0,self.i)
+        for cycle1 in range(self.i-2):
+            Y1[cycle1+1]=2*(1-r**2)*self.Y0[cycle1+1]-self.Y0[cycle1+1]+r**2*(self.Y0[cycle1+2]+self.Y0[cycle1])
+        self.Y.append(Y1)
+        while not len(self.Y) > (self.n+1):
+            newY=np.linspace(0,0,self.i)
+            for cycle2 in range(self.i-2):
+                newY[cycle2+1]=2*(1-r**2)*self.Y[-1][cycle2+1]-self.Y[-2][cycle2+1]+r**2*(self.Y[-1][cycle2+2]+self.Y[-1][cycle2])
+            self.Y.append(newY)
+        return None
+    def plot(self,i):
+        plt.plot(self.X,self.Y[i])
+        return 0
+    def movie(self):
+        # New figure with white background
+        fig = plt.figure(figsize=(6,6), facecolor='white')
+        # New axis over the whole figure, no frame and a 1:1 aspect ratio
+        ax = fig.add_axes([0,0,1,1], frameon=False, aspect=1)
+        line, = ax.plot([], [], lw=2)
+        def init():  
+            line.set_data([], [])  
+            return line,
+        def animate(i):
+            x = self.X
+            y = self.Y[i]
+            line.set_data(x, y)	  
+            return line,
+        anim1=animation.FuncAnimation(fig, animate, init_func=init, frames=50, interval=10)
+        plt.show()
+
+A=wave()
+A.calculate()
+#A.plot(1)
+#print A.Y
+A.movie()
